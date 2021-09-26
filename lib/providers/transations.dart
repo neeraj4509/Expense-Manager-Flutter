2c3ebd9bcca8a118a4ac3f3508ee646eb07dd62f
neeraj4509 <neerajbendkhale@gmail.com>
2021-09-26 09:23:17 +0530

    this is first commit

---
+++ lib/providers/transations.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter/material.dart';
+
+// class Transaction{
+//   final String title;
+//   final double value;
+//   final String category;
+//   final IconData iconData;
+
+//   const Transaction({
+//     required this.title, 
+//     required this.value, 
+//     required this.category, 
+//     required this.iconData,
+//     });
+// }
